6f632f531b5ac2625e0fa3420048cdc39010f847
totosha16 <totosha16@mail.ru>
2016-07-16 22:17:43 +0300

    2nd Done in Test

--- testNok.rb
+++ testNok.rb
@@ -1,22 +1,40 @@
 require 'nokogiri'
 require 'open-uri'
 
-url="http://lomrus.ru/tseny-na-metallolom"
+url="http://rosmet-lom.ru/"
 html = Nokogiri::HTML(open(url))
 
-#f0= File.new("all.txt", "w")
-f= File.new("string.txt", "w")
-f1= File.new("all.txt", "w")
+f0= File.new("all.txt", "w")
+f1= File.new("step1.txt", "w")
+f2= File.new("step2.txt", "w")
+f3= File.new("step3.txt", "w")
 
-html.css("#print-content").each do |node|
-	f1.write node
-	section_html = Nokogiri::HTML(node.inner_html)
 
-	price = section_html.css('td')[5].text.gsub(/\D/,"").to_i
-	date = html.css(".dali-postcontent h2").text.gsub(/\D/,"")
+f2.write html.css(".tab1 tbody tr")[1].css("td")[2].text.gsub(/\D/,"").to_i
+#f2.write html.xpath("@0x148bd6c").text
+f3.write html.at_css(".tab1 tbody").inspect
+
+html.css(".tab1 tbody").each do |part|
+	f0.write part
 	
-	f.write "#{url}, #{price}, #{date}\r"
-	
+#	section_html = Nokogiri::HTML(node.inner_html)
+
+
+	price = part.at_css('td').text#.gsub(/\D/,"").to_i
+	f1.write price
+#	price.each do |qq|
+#	f.write qq
+#	end
+
+#	f.write "#{url}, #{price}\r"
+
 end
-f.close
+
+
+
+f0.close
+f1.close
+f2.close
+f3.close
+
 gets
